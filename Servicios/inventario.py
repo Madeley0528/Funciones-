7c0fe784d4d2439f3dd467adaeb94ace75a65844
Madeley0528 <mdlorenzo@scl.edu.gt>
2024-10-11 12:16:45 -0600

    Update inventario.py

---
+++ Servicios/inventario.py
@@ -0,0 +1,7 @@
+inventario = [
+    {"nombre": "Manzanas", "cantidad": 50, "precio": 2.50},
+    {"nombre": "Bananas", "cantidad": 30, "precio": 1.75},
+    {"nombre": "Leche", "cantidad": 20, "precio": 5.00},
+    {"nombre": "Pan", "cantidad": 40, "precio": 3.00},
+    {"nombre": "Huevos", "cantidad": 60, "precio": 0.50}
+]
