a0f75c589956fad0eb89b39418342adedca3cb76
Madeley0528 <mdlorenzo@scl.edu.gt>
2025-03-10 20:47:21 -0600

    Delete Servicios directory

--- Servicios/inventario.py
+++
@@ -1,8 +0,0 @@
-inventario = [
-    {"nombre": "Té", "cantidad": 40, "precio": 8.00},
-    {"nombre": "Galletas", "cantidad": 100, "precio": 2.50},
-    {"nombre": "Pasta", "cantidad": 60, "precio": 3.75},
-    {"nombre": "Aceite", "cantidad": 25, "precio": 15.00},
-    {"nombre": "Salsa de tomate", "cantidad": 30, "precio": 4.00}
-]
-
