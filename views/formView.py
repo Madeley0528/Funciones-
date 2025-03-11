8fb455332a5fcba0200f1a0eae2a76aba579cf2d
Madeley0528 <mdlorenzo@scl.edu.gt>
2025-03-11 12:06:02 -0600

    Add files via upload

--- views/formView.py
+++ views/formView.py
@@ -1,23 +1,25 @@
+#fromView.py
 import tkinter
-from datosView import actualizarTabla
-def formView(ventana):
-    formulario_panel = tkinter.Frame(ventana, bg="blue", width="300", height="600") 
+from views.datosView import actualizarTabla
+
+def form_view(ventana):
+    formulario_panel = tkinter.Frame(ventana, bg="yellow", width="300", height="600") 
     formulario_panel.grid(row=1, column=0, sticky="nsew")
 
-    def funcion_boton():
-        respuesta = entry.get()
-        print(respuesta)
-
-    tabla_panel = tkinter.Frame(ventana, bg="sky blue", width="700", height="600") 
+    tabla_panel = tkinter.Frame(ventana, bg="green", width="700", height="600") 
     tabla_panel.grid(row=1, column=1, sticky="nsew")
-    #actualizarTabla(f"SELECT {respuesta} FROM datos_generales", tabla_panel)
-
 
     titulo = tkinter.Label(formulario_panel, text="Formulario de registro")
     titulo.pack(pady=5)
 
     entry = tkinter.Entry(formulario_panel)
     entry.pack(pady=5)
+
+    def funcion_boton():
+        respuesta = entry.get()
+        print(respuesta)
+
+        actualizarTabla(f"SELECT {respuesta} FROM Carros", tabla_panel)
 
     boton = tkinter.Button(formulario_panel, text="Enviar", command=funcion_boton)
     boton.pack(pady=5)
